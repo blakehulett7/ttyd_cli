651adb0ddcb9a41cd748cf317527417e0eb6d21a
blakehulett7 <blake.hulett7@gmail.com>
2024-06-08 16:06:59 -0500

    Formatter working okay but had to fix an ellipses bug. Decent upfront investment, but should make cutscenes easier to make. Will finish opening cutscene shortly.

--- src/cutscenes.py
+++ src/cutscenes.py
@@ -1,3 +1,6 @@
+from graphics import text_formatter
+
+
 def opening_sequence():
     print("Welcome to TTYD!")
     input("Always press ENTER to continue...")
@@ -20,6 +23,28 @@ def goombella_crump_sequence():
     input()
     print("Girl: Hey! What do you want?!? Get away from me, freak!!!")
     input()
-    print("Freak: Oh, come off it, you airhead! I know it's tough for you, but don't play dumb with me!")
+    print("Freak:")
     input()
-    print("Freak: I've seen you walking around town asking about the Crystal Stars.")
+    text_formatter("""Oh, come off it, you airhead! I know it's tough for you, but don't
+            play dumb with me! I've seen you walking around town asking for
+            information about the Crystal Stars. Well, now I'm doing the
+            asking, so be a good girl and tell us what you know! Right. NOW!""")
+    print("Girl: Never! I don't have anything to say to you creeps! Eww!")
+    input()
+    print("Freak:")
+    input()
+    text_formatter("""I suppose it wouldn't be right if a sassy little lass like you met 
+            with an untimely demise... Buh! Buh! Buh huh huh! Boys, we're
+                   taking this firebrand to our fortress!""")
+    print("Creep: As you command, Lord Crump! We're on it!")
+    input()
+    print("Lord Crump and his creeps get closer to the girl.")
+    input()
+    print("Girl: N-No! Stop right there, you weirdos! I'll scream! Really!")
+    input()
+    print("Mario walks up to them, and the Goomba girl takes cover behind him")
+    input()
+    print("Girl: Like I'd go anywhere with smelly lunatics like you! Hmph! Not likely!")
+
+
+goombella_crump_sequence()
--- src/graphics.py
+++ src/graphics.py
@@ -10,15 +10,31 @@ def text_formatter(text):
     number_of_boxes = len(raw_text) // 80 + 1
     line_size = math.ceil(raw_text_size / number_of_boxes)
     boxes = []
-    # Recursive function here?
-    first_period_index = raw_text[line_size:].index(".") + line_size
-    first_exclamation_index = raw_text[line_size:].index("!") + line_size
-    dividing_index = min(first_period_index, first_exclamation_index) + 1
-    first_box = raw_text[:dividing_index]
-    return raw_text, first_box
-
-
-print(text_formatter("""Oh, come off it, you airhead! I know it's tough for you, but don't
-            play dumb with me! I've seen you walking around town asking for
-            information about the Crystal Stars. Well, now I'm doing the
-            asking, so be a good girl and tell us what you know! Right. NOW!"""))
+    for i in range(number_of_boxes):
+        if len(raw_text) < line_size:
+            boxes.append(raw_text)
+        else:
+            if "..." in raw_text[line_size:]:
+                dividing_index = raw_text[line_size:].index(
+                    "...") + 3 + line_size
+            else:
+                first_period_index = float("inf")
+                first_exclamation_index = float("inf")
+                if "." in raw_text[line_size:]:
+                    first_period_index = raw_text[line_size:].index(
+                        ".") + line_size
+                if "!" in raw_text[line_size:]:
+                    first_exclamation_index = raw_text[line_size:].index(
+                        "!") + line_size
+                dividing_index = min(first_period_index,
+                                     first_exclamation_index) + 1
+            if dividing_index == float("inf"):
+                boxes.append(raw_text)
+            else:
+                first_box = raw_text[:dividing_index]
+                boxes.append(first_box)
+            raw_text = raw_text[dividing_index:].lstrip()
+    for box in boxes:
+        if box != "":
+            print(box)
+            input()
