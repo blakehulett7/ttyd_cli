fa216939eb14ad0a9d0854cc17a90fdf551e3b24
blakehulett7 <blake.hulett7@gmail.com>
2024-06-08 22:39:30 -0500

    Small rewrite to take the recursion out. Was returning things i didn't want to return. Mario was getting too many turns!!

--- src/logic.py
+++ src/logic.py
@@ -21,47 +21,36 @@ class Mario:
         return f"(Mario! {self.hp} hp, {self.fp} fp, {self.bp} bp)"
 
     def turn(self, enemy_list):
-        print("")
-        print("-----Mario's Turn-----")
-        for move in self.moves_list:
-            i = self.moves_list.index(move) + 1
-            print(f"{i}. {move.name}")
-        print("")
-        command = input("Choose an action: ")
-        if (not command.isdigit()
-            or int(command) - 1 < 0
-                or int(command) - 1 >= len(self.moves_list)):
-            print("\nInvalid command, try again.")
-            self.turn(enemy_list)
-        else:
-            move_index = int(command)
-        if move_index == 1:
-            print("\nYou've selected jump")
-            selection = self.target_selection(enemy_list)
-            self.battle_jump(selection)
-        elif move_index == 2:
-            print("\nYou've selected hammer!")
-            confirmation = 0
-            while confirmation not in ["Y", "y", "N", "n"]:
-                confirmation = input(f"You will target {
-                    enemy_list[0].name}, is this ok?(Y/n): ")
-            if confirmation in ["N", "n"]:
-                self.turn(enemy_list)
-            elif confirmation in ["Y", "y"]:
-                self.battle_hammer(enemy_list[0])
+        command = "None"
+        while not command.isdigit() or int(command) not in [i for i in range(1, len(enemy_list) + 2)]:
+            print("")
+            print("-----Mario's Turn-----")
+            for move in self.moves_list:
+                i = self.moves_list.index(move) + 1
+                print(f"{i}. {move.name}")
+            print("")
+            command = input("Choose an action: ")
+        if int(command) == 1:
+            print("Jump")
+        if int(command) == 2:
+            print("Hammer")
 
     def target_selection(self, enemy_list):
         confirmation = "None"
         while confirmation not in ["Y", "y"]:
             command = "None"
-            while not command.isdigit() or int(command) - 1 not in [i for i in range(len(enemy_list))]:
+            while not command.isdigit() or int(command) - 1 not in [i for i in range(len(enemy_list) + 1)]:
                 print("")
                 print("-----Enemies-----")
                 for enemy in enemy_list:
                     i = enemy_list.index(enemy) + 1
                     print(f"{i}. {enemy.name}")
+                i += 1
+                print(f"{i}. Go back")
                 print("")
                 command = input("Choose a target: ")
+            if int(command) == i:
+                return None
             index = int(command) - 1
             selection = enemy_list[index]
             confirmation = input(f"\nYou will target {
