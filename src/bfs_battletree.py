45e7c1c04cc8f80650ba21285b06c7db640238ca
blakehulett7 <blake.hulett7@gmail.com>
2024-06-14 17:02:38 -0500

    Progress

--- src/bfs_battletree.py
+++ src/bfs_battletree.py
@@ -3,34 +3,64 @@ from graphics import Colors
 
 class Battletree:
     def __init__(self):
-        self.nil = Battlenode(None)
-        self.root = Battlenode("Root")
-        self.val = "Test"
+        self.root = None
 
     def __repr__(self):
         return f"{self.root}"
 
-    def create_tree(self, mario, partner_list, enemy_list, current_node):
+    def create_tree(self, mario, partner_list, enemy_list, current_node=None):
         partner = partner_list[0]
         formation = [mario, partner]
+        if current_node is None:
+            self.root = Rootnode()
+            self.create_tree(mario, partner_list, enemy_list, self.root)
         if current_node == self.root:
+            next_layer = []
             for good_guy in formation:
-                self.root.children.append(Battlenode(good_guy))
-        elif hasattr(current_node.val, "moves_list"):
-            for move in current_node.val.moves_list:
-                current_node.children.append(Battlenode(move))
-        else:
-            for enemy in enemy_list:
-                current_node.children.append(Battlenode(enemy))
+                for move in good_guy.moves_list:
+                    for enemy in enemy_list:
+                        next_layer.append(Battlenode(
+                            mario,
+                            partner_list,
+                            enemy_list,
+                            "good",
+                            1,
+                            good_guy,
+                            move,
+                            enemy
+                        ))
+            current_node.children.extend(next_layer)
+        '''
         for child in current_node.children:
             self.create_tree(mario, partner_list, enemy_list, child)
+        '''
 
 
 class Battlenode:
-    def __init__(self, val):
+    def __init__(self, mario, partner_list, enemy_list, team, position, user, move, target):
         self.parent = None
         self.children = []
-        self.val = val
+        self.val = f"{user.name} uses {move} on {target.name}"
+        self.mario = mario
+        self.partner_list = partner_list
+        self.enemy_list = enemy_list
+        self.team = team
+        self.position = position
+        self.user = user
+        self.move = move
+        self.target = target
+
+    def __repr__(self, level=0):
+        ret = "\t"*level+repr(self.val)+"\n"
+        for child in self.children:
+            ret += child.__repr__(level+1)
+        return ret
+
+
+class Rootnode:
+    def __init__(self):
+        self.children = []
+        self.val = "Battle Start"
 
     def __repr__(self, level=0):
         ret = "\t"*level+repr(self.val)+"\n"
